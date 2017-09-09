914df009691d947e01904133b528d48b95c11abc
naveenvpai <pai.naveen357@gmail.com>
2017-09-09 00:12:22 -0400

    Add primitive story writing

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(http);
 
-const max_room_size = 10;
-const min_room_size = 3;
-const max_word_count = 300;
-const min_archivable_word_count = 200;
-const max_words_per_turn = 3;
-const max_chars_per_turn = 5;
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on("connection", socket => {
-	socket.emit("connection", socket.player);
-
-	socket.on("evt", () => {
-
+io.on('connection', function(socket){
+	console.log("connected");
+	socket.on("snippet", function(snip) {
+		console.log('got it');
+		io.emit("snippet", snip);	
 	});
 });
 
 http.listen(port, () => {
 	console.log(`Listening on ${port}.`);
 });
+
+const max_room_size = 10;
+const min_room_size = 3;
+const max_word_count = 300;
+const min_archivable_word_count = 200;
+const max_words_per_turn = 3;
+const max_chars_per_turn = 5;
