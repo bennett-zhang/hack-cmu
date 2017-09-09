d01fd884612bf5b17822063a3c198c9a5821cdb4
naveenvpai <pai.naveen357@gmail.com>
2017-09-08 21:41:19 -0400

    Add constants to socket server

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(http);
 
+const max_room_size = 10;
+const min_room_size = 3;
+const max_word_count = 300;
+const min_archivable_word_count = 200;
+const max_words_per_turn = 3;
+const max_chars_per_turn = 5;
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
